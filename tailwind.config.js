cf4426e64ce9cce07a56046f81558b3c6863e1d8
0thw0foxa0 <vladboxlive@mail.ru>
2024-04-04 15:14:59 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  corePlugins: {
+    preflight: false,
+  },
+
+  important : true,
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
