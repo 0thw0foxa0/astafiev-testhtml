e43813f7cc46947e9bf7ed248759a165b8dbbb08
0thw0foxa0 <vladboxlive@mail.ru>
2024-06-16 21:19:06 +0700

    zat3si

--- src/preload.js
+++ src/preload.js
@@ -7,3 +7,22 @@ window.onload = function() {
     }, '500');
    
 };
+
+
+function changeImage() {
+    console.log("da");
+    var play = document.getElementById('play');
+    var stop = document.getElementById('stopplay');
+    var video = document.getElementById('bgvideo');
+
+    if (play.style.display !== 'none') {
+        video.muted = !video.muted;
+        play.style.display = 'none'
+        stop.style.display = 'block';
+    }
+    else {
+        video.muted = "true";
+        play.style.display = 'block';
+        stop.style.display = 'none';
+    }
+};
