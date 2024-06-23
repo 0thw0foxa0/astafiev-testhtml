ee1026b4b0238c2f96f58ed313fa961be31a65f2
0thw0foxa0 <vladboxlive@mail.ru>
2024-06-23 14:55:51 +0700

    1

--- src/preload.js
+++ src/preload.js
@@ -1,11 +1,9 @@
 window.onload = function() {
-    
     document.getElementById("preloader").classList.add("opacity-0")
     document.getElementById("preloader").classList.remove("opacity-100")
     setTimeout(() => {
         document.getElementById('preloader').remove()
     }, '500');
-   
 };
 
 
